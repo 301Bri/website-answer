27df53a0a690a557fae83ed804c0ffa259de0525
301Bri <90125708+301Bri@users.noreply.github.com>
2024-02-23 20:00:33 +0800

    Create logout.js

---
+++ views/logout.js
@@ -0,0 +1,8 @@
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+    }
+    res.redirect('/');
+  });
+});
